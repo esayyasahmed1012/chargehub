6183b4122c4b18c6baf8dc1fcffee61558115395
esayyas ahmed <esayyasahmed1012@gmail.com>
2024-09-23 20:20:05 -0700

    .\app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const auth_routes = require('./routes/auth_routes');
+const station_routes = require('./routes/station_routes');
+const session_routes = require('./routes/session_routes');
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+
+// routes
+app.use('/auth', auth_routes);
+// Charging Station Routes
+app.use('/api', station_routes);
+
+// Charging Session Routes
+app.use('/api', session_routes);
+// home route
+app.get('/', (req, res) => {
+    res.send('Welcome to ChargeHub Backend API');
+});
+
+
+// Only start the server if this file is run directly
+if (require.main === module) {
+    const PORT =  3000; 
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+
+module.exports = app;
