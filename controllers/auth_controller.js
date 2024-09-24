d8e05848117320cb30f04c59bca1e7fd008627ad
esayyas ahmed <esayyasahmed1012@gmail.com>
2024-09-23 20:21:13 -0700

    .\auth_controller.js

---
+++ controllers/auth_controller.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Register a new user
+exports.register = async (req, res) => {
+  const { username, email, password, phone_number, role } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.register(username, email, hashedPassword, phone_number, role);
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user', error });
+  }
+};
+
+// Login a user
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByEmail(email);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user.user_id, role: user.role }, process.env.SECRET_KEY, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Error logging in', error });
+  }
+};
