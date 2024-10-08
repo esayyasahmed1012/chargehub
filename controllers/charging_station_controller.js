f8c143d4b6169ccd7e0d37ff81bca5165ed573a3
esayyas ahmed <esayyasahmed1012@gmail.com>
2024-10-07 19:33:41 -0700

    .\charging_station_controller.js

--- controllers/charging_station_controller.js
+++ controllers/charging_station_controller.js
@@ -2,10 +2,19 @@ const charging_station_model = require('../models/charging_station_model');
 
 // Create a new charging station
 exports.createStation = async (req, res) => {
-  const { station_name, location } = req.body;
+  const { station_name, latitude, longitude } = req.body;
   try {
-    await charging_station_model.create(station_name, location);
-    res.status(201).json({ message: 'Charging station created successfully' });
+    if (!station_name || !latitude || !longitude) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+    const existingStation = await charging_station_model.findByStationName(station_name);
+    if (existingStation) {
+      console.log("station already exist")
+      return res.status(400).json({ message: 'Station already exists' });
+    }
+   await charging_station_model.create(station_name, latitude, longitude);
+   res.status(201).json({ message: 'Charging station created successfully' });
+   
   } catch (error) {
     res.status(500).json({ message: 'Error creating charging station', error });
   }
